c89a85ec852eecace0e51f4cd7b638dcdf01c819
harmandeep2993 <harmandeep2993@gmail.com>
2026-04-01 00:45:00 +0200

    feat: add main pipeline entry point

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from movie-recommendation-system-mlops!")
+from src.data import load_dataset
+from src.data import preprocess_pipeline
+from src.features import build_features_pipeline
+from src.models.train import train_pipeline
 
+# step 1 - load
+datasets = load_dataset()
 
-if __name__ == "__main__":
-    main()
+# step 2 - preprocess
+ratings_train, ratings_test, movies, users = preprocess_pipeline(
+    datasets["ratings"],
+    datasets["movies"],
+    datasets["users"]
+)
+
+# step 3 - build features
+user_item_matrix, user_map, item_map = build_features_pipeline(ratings_train)
+
+# Step 4 - train model
+best_model = train_pipeline(user_item_matrix, test=ratings_test, user_map=user_map, item_map=item_map, k_values=[10, 20, 50])
+
+print("Training completed successfully!")
